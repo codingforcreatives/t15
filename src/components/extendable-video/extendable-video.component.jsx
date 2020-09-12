73bede5c47731a62eaffa42449a63fb31c890c4a
Clarentia Mursalim <clary@Clarentias-MacBook-Pro.local>
2020-09-13 00:59:38 +1000

    image still implemented

--- src/components/extendable-video/extendable-video.component.jsx
+++ src/components/extendable-video/extendable-video.component.jsx
@@ -6,14 +6,20 @@ import { Transition } from 'react-transition-group';
 
 const ExtendableVideo = () => {
 	const [width, setWidth] = useState('395.62px');
-	const [expandedClass, setExpandedClass] = useState('initial');
+	const [expanded, setExpanded] = useState(false);
 	return (
 		<div
 			id="extendable-box"
 			style={{ width: width }}
-			onMouseEnter={() => setWidth('40%')}
-			onMouseLeave={() => setWidth('395.62px')}>
-			<div id="left-triangle">
+			onMouseEnter={() => {
+				setWidth('40%');
+				setExpanded(true);
+			}}
+			onMouseLeave={() => {
+				setWidth('395.62px');
+				setExpanded(false);
+			}}>
+			{/* <div id="left-triangle">
 				<svg id="left-triangle" viewBox="0 0 201 493">
 					<path
 						id="Path_36"
@@ -22,18 +28,25 @@ const ExtendableVideo = () => {
 						d="M-0.3,493.2L-0.3,493.2V0.2h201L-0.3,493.2z"
 					/>
 				</svg>
-			</div>
-			<video
-				autoPlay="autoPlay"
-				muted
-				loop="loop"
-				className="extendable-video-background"
-				mask="url(#clipPath)">
-				<source
-					src={require('../../assets/Website-Hero-Compressed-v2.mp4')}
-					type="video/mp4"
-				/>
-			</video>
+			</div> */}
+			{expanded == true ? (
+				<video
+					autoPlay="autoPlay"
+					muted
+					loop="loop"
+					className="extendable-video-background"
+					mask="url(#clipPath)">
+					<source
+						src={require('../../assets/Website-Hero-Compressed-v2.mp4')}
+						type="video/mp4"
+					/>
+				</video>
+			) : (
+				<img
+					className="still-image"
+					src={require('../../assets/temporary-screenshot.png')}></img>
+			)}
+			{/* 
 			<div id="right-triangle">
 				<svg id="right-triangle" viewBox="0 0 201 493">
 					<path
@@ -43,7 +56,7 @@ const ExtendableVideo = () => {
 						d="M200.7,0.2L200.7,0.2v493h-201L200.7,0.2z"
 					/>
 				</svg>
-			</div>
+			</div> */}
 		</div>
 	);
 };
