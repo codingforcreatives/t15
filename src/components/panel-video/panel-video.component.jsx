01ff8a7567ce6bd7cf8e4722aacf0c9ddf462ac2
Clarentia Mursalim <clary@192-168-1-105.tpgi.com.au>
2021-01-15 18:11:06 +1100

    panel video fixed

--- src/components/panel-video/panel-video.component.jsx
+++ src/components/panel-video/panel-video.component.jsx
@@ -1,48 +1,44 @@
 import React, { useRef, useState, useEffect } from 'react';
-import './panel-video.styles.scss';
 import ReactPlayer from 'react-player';
+const styles = require('./panel-video.module.css');
 
-const PanelVideo = ({ videoURL }) => {
-	let featureVideo = useRef(null);
-	let featureVideoContainer = useRef(null);
-	let url = videoURL;
-	console.log('VIDEO URL');
-	console.log(url);
+const PanelVideo = ({ videoName }) => {
+	// let url = videoURL;
+
 	return (
-		<div
-			className="featureVideoComponentContainer"
-			ref={(el) => (featureVideoContainer = el)}>
-			<img
-				className="video-mask"
-				src={require(`../../assets/feature-overlay.png`)}></img>
-			{/* <video
-				ref={(el) => (featureVideo = el)}
-				autoPlay="autoPlay"
-				muted
-				loop="loop"
-				className="feature-video">
-				Your browser does not support the video tag. I suggest you upgrade your
-				browser
-				<source src={require(`../../assets/${videoName}`)} type="video/mp4" />
-			</video> */}
-
-			<ReactPlayer
-				config={{
-					vimeo: {
-						playerOptions: {
-							autoplay: true,
-							controls: false,
-							loop: true,
-							muted: true,
+		<>
+			<div className={styles.panelVideo}>
+				<img
+					className={styles.panelOverlay}
+					src={require(`../../assets/feature-overlay-6.png`)}
+				/>
+				{/* <ReactPlayer
+					config={{
+						vimeo: {
+							playerOptions: {
+								autoplay: true,
+								controls: false,
+								loop: true,
+								muted: true,
+							},
 						},
-					},
-				}}
-				url={url}
-				width="100%"
-				height="100%"
-				className="feature-video"
-				ref={(el) => (featureVideo = el)}></ReactPlayer>
-		</div>
+					}}
+					url={url}
+					width="100%"
+					height="100%"
+					// className={styles.panelVideo}
+				/> */}
+
+				<video
+					autoPlay="autoPlay"
+					muted
+					loop="loop"
+					className="feature-video"
+					mask="url(#clipPath)">
+					<source src={require(`../../assets/${videoName}`)} type="video/mp4" />
+				</video>
+			</div>
+		</>
 	);
 };
 
