9e24ef49faa2972b4a7cca6eb88ab70a8e5472f6
nipoonp <patel_nipoon@yahoo.co.nz>
2021-01-17 01:50:32 +1300

    Move sections into different components

--- src/pages/video-content-items/travel-tourism/travel-tourism-page.component.jsx
+++ src/pages/video-content-items/travel-tourism/travel-tourism-page.component.jsx
@@ -2,14 +2,11 @@ import React, { useRef, useEffect, useState } from 'react';
 
 import axios from 'axios';
 import PanelVideo from '../../../components/panel-video/panel-video.component';
-import PortfolioIntroductionSection from '../../../components/portfolio-introduction-section/portfolio-introduction-section.component';
-
-import ReactPlayer from 'react-player';
 import VimeoVideo from '../../../components/vimeo-video/vimeo-video.component';
 import { useKeenSlider } from 'keen-slider/react';
 import 'keen-slider/keen-slider.min.css';
 
-import { TweenMax, Power3, Power4, TimelineLite } from 'gsap';
+import { TimelineLite } from 'gsap';
 const styles = require('./travel-tourism.module.css');
 
 // post ids:
@@ -19,29 +16,29 @@ const styles = require('./travel-tourism.module.css');
 const TravelTourismPage = () => {
 	const [isLoaded, setLoaded] = useState(false);
 
-	let introduction = {
+	const [introduction, setIntroduction] = useState({
 		title: '',
 		description: '',
 		video: '',
-	};
-	let item1 = {
-		title: '',
-		description: '',
-		video: '',
-		gallery: '',
-	};
-	let item2 = {
+	});
+	const [item1, setItem1] = useState({
 		title: '',
 		description: '',
 		video: '',
 		gallery: '',
-	};
-	let item3 = {
+	});
+	const [item2, setItem2] = useState({
 		title: '',
 		description: '',
 		video: '',
 		gallery: '',
-	};
+	});
+	const [item3, setItem3] = useState({
+		title: '',
+		description: '',
+		video: '',
+		gallery: '',
+	});
 
 	//Gsap Animations
 	let backgroundVideo = useRef(null);
@@ -50,30 +47,31 @@ const TravelTourismPage = () => {
 	const tl = new TimelineLite();
 
 	let handleData = (data) => {
-		introduction = {
+		setIntroduction({
 			title: data.acf.introduction_title,
 			description: data.acf.introduction_description,
 			video: data.acf.introduction_video,
-		};
+		});
 
-		item1 = {
+		setItem1({
 			title: data.acf.item_1_title,
 			description: data.acf.item_1_description,
 			video: data.acf.item_1_video,
 			gallery: data.acf.item_1_gallery,
-		};
-		item2 = {
+		});
+
+		setItem2({
 			title: data.acf.item_2_title,
 			description: data.acf.item_2_description,
 			video: data.acf.item_2_video,
 			gallery: data.acf.item_2_gallery,
-		};
-		item3 = {
+		});
+		setItem3({
 			title: data.acf.item_3_title,
 			description: data.acf.item_3_description,
 			video: data.acf.item_3_video,
 			gallery: data.acf.item_3_gallery,
-		};
+		});
 		setLoaded(true);
 		console.log('handled data');
 		console.log(introduction);
@@ -87,7 +85,7 @@ const TravelTourismPage = () => {
 
 		console.log('MADE API CALL');
 		console.log(introduction);
-	});
+	}, []);
 
 	// return (
 	// 	<div ref={(el) => (homepageContainer = el)} className="portfolio-container">
@@ -115,6 +113,105 @@ const TravelTourismPage = () => {
 		},
 	})
 
+	const IntroductionSection = (
+		<>
+			<div className={styles.headerTextContainer} >
+				<div className={styles.headerTextLeftSection}>
+					<div className={styles.heading1}>{introduction.title}</div>
+					<div className={styles.space2}></div>
+					<div>{introduction.description}</div>
+				</div>
+
+				<div className={styles.headerTextRightSection}>
+					<div className={styles.heading3}>LATEST CLIENTS</div>
+					<div className={styles.space2}></div>
+					<div>{item1.title}</div>
+					<div>{item2.title}</div>
+					<div>{item3.title}</div>
+				</div>
+			</div >
+		</>
+	)
+
+	const Item1Section = (
+		<div className={styles.item1Container} >
+			<div className={styles.heading2}>{item1.title}</div>
+			<div className={styles.space2}></div>
+			<div>{item1.description}</div>
+			<div className={styles.space3}></div>
+
+			<div className={styles.vimeoVideoContainer}>
+				<VimeoVideo videoURL={item1.video} />
+			</div>
+		</div>
+	)
+
+	const Item2Section = (
+		<div className={styles.item2Container} >
+			<div className={styles.heading2}>{item2.title}</div>
+			<div className={styles.space2}></div>
+			<div>{item2.description}</div>
+			<div className={styles.space3}></div>
+
+			<div className={styles.vimeoVideoContainer}>
+				<VimeoVideo videoURL={item2.video} />
+			</div>
+		</div>
+	)
+
+	const Item3Section = (
+		<div className={styles.item3Container} >
+			<div className={styles.heading2}>{item3.title}</div>
+			<div className={styles.space2}></div>
+			<div>{item3.description}</div>
+			<div className={styles.space3}></div>
+
+			<div className={styles.vimeoVideoContainer}>
+				<VimeoVideo videoURL={item3.video} />
+			</div>
+		</div>
+	)
+
+	const SliderSection = (
+		<div className={styles.navigationWrapper} >
+			<div ref={sliderRef} className="keen-slider">
+				<div className="keen-slider__slide">
+					<img
+						className={styles.keenSlide}
+						src={item1.gallery}
+					/>
+				</div>
+				<div className="keen-slider__slide">
+					<img
+						className={styles.keenSlide}
+						src={item2.gallery}
+					/>
+				</div>
+				<div className="keen-slider__slide">
+					<img
+						className={styles.keenSlide}
+						src={item3.gallery}
+					/>
+				</div>
+			</div>
+			{
+				slider && (
+					<>
+						<ArrowLeft
+							onClick={(e) => e.stopPropagation() || slider.prev()}
+							disabled={currentSlide === 0}
+						/>
+						<ArrowRight
+							onClick={(e) => e.stopPropagation() || slider.next()}
+							disabled={currentSlide === slider.details().size - 1}
+						/>
+					</>
+				)
+			}
+		</div>
+	)
+
+
 	return (
 		<>
 			<div className={styles.outerContainer}>
@@ -122,116 +219,13 @@ const TravelTourismPage = () => {
 					<div className={styles.featureVideoContainer}>
 						<PanelVideo videoName="Masci_HQ.mp4" />
 					</div>
-					<div className={styles.headerTextContainer}>
-						<div className={styles.headerTextLeftSection}>
-							<div className={styles.heading1}>Travel & Adventure</div>
-							<div className={styles.space2}></div>
-							<div>
-								We are specialists in helping businesses deliver their stories
-								in the most creative ways possible.From full-in-house production
-								to strategy and creative treatment, you have come to the right
-								place. We’ve got you.
-							</div>
-						</div>
 
-						<div className={styles.headerTextRightSection}>
-							<div className={styles.heading3}>LATEST CLIENTS</div>
-							<div className={styles.space2}></div>
-							<div>Air New Zealand</div>
-							<div>Polar Pro</div>
-							<div>Mui Campervans</div>
-						</div>
-					</div>
-
-					<div className={styles.item1Container}>
-						<div className={styles.heading2}>AIR NEW ZEALAND</div>
-						<div className={styles.space2}></div>
-						<div>
-							We are specialists in helping businesses deliver their stories in
-							the most creative ways possible.From full-in-house production to
-							strategy and creative treatment, you have come to the right place.
-							We’ve got you.
-						</div>
-						<div className={styles.space3}></div>
-
-						<div className={styles.vimeoVideoContainer}>
-							<VimeoVideo videoURL={'https://vimeo.com/326463076'} />
-						</div>
-
-						<div className={styles.space3}></div>
-						<div className={styles.navigationWrapper}>
-							<div ref={sliderRef} className="keen-slider">
-								<div className="keen-slider__slide">
-									<img
-										className={styles.keenSlide}
-										src={
-											'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png'
-										}
-									/>
-								</div>
-								<div className="keen-slider__slide">
-									<img
-										className={styles.keenSlide}
-										src={
-											'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png'
-										}
-									/>
-								</div>
-								<div className="keen-slider__slide">
-									<img
-										className={styles.keenSlide}
-										src={
-											'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png'
-										}
-									/>
-								</div>
-							</div>
-							{slider && (
-								<>
-									<ArrowLeft
-										onClick={(e) => e.stopPropagation() || slider.prev()}
-										disabled={currentSlide === 0}
-									/>
-									<ArrowRight
-										onClick={(e) => e.stopPropagation() || slider.next()}
-										disabled={currentSlide === slider.details().size - 1}
-									/>
-								</>
-							)}
-						</div>
-					</div>
-
-					<div className={styles.item2Container}>
-						<div className={styles.heading2}>POLAR PRO</div>
-						<div className={styles.space2}></div>
-						<div>
-							We are specialists in helping businesses deliver their stories in
-							the most creative ways possible.From full-in-house production to
-							strategy and creative treatment, you have come to the right place.
-							We’ve got you.
-						</div>
-						<div className={styles.space3}></div>
-
-						<div className={styles.vimeoVideoContainer}>
-							<VimeoVideo videoURL={'https://vimeo.com/326463076'} />
-						</div>
-					</div>
-
-					<div className={styles.item3Container}>
-						<div className={styles.heading2}>MAUI CAMPERVANS</div>
-						<div className={styles.space2}></div>
-						<div>
-							We are specialists in helping businesses deliver their stories in
-							the most creative ways possible.From full-in-house production to
-							strategy and creative treatment, you have come to the right place.
-							We’ve got you.
-						</div>
-						<div className={styles.space3}></div>
-
-						<div className={styles.vimeoVideoContainer}>
-							<VimeoVideo videoURL={'https://vimeo.com/326463076'} />
-						</div>
-					</div>
+					{IntroductionSection}
+					{Item1Section}
+					<div className={styles.space3}></div>
+					{SliderSection}
+					{Item2Section}
+					{Item3Section}
 				</div>
 			</div>
 		</>
@@ -239,12 +233,6 @@ const TravelTourismPage = () => {
 };
 
 export default TravelTourismPage;
-
-/* <div className="featureVideoContainer">
-				<PanelVideo videoURL="https://vimeo.com/326463076" />
-			</div> */
-
-
 
 const ArrowLeft = (props) => {
 	return (
