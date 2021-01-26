f3179b896a18d459395c1bfdc4ab6924b192c74b
Clarentia Mursalim <clary@192-168-1-105.tpgi.com.au>
2021-01-27 01:16:05 +1100

    added standby gif for pane video

--- src/components/panel-video/panel-video.component.jsx
+++ src/components/panel-video/panel-video.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { S3_BASE_URL, PANEL_VIDEO_OVERLAY } from '../../components/globals';
+import logo from '../../assets/standby.gif';
 const styles = require('./panel-video.module.css');
 
 const PanelVideo = ({ videoName }) => {
@@ -7,6 +8,7 @@ const PanelVideo = ({ videoName }) => {
 		<>
 			<div className={styles.panelVideo}>
 				<img className={styles.panelOverlay} src={PANEL_VIDEO_OVERLAY} />
+				<img className={styles.standBy} src={logo} alt="loading..." />
 
 				<video
 					autoPlay="autoPlay"
