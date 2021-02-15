ab47a9316219b45f6717cf1214f332cedf317ed2
Clarentia Mursalim <clarentia@paperballoon.co>
2021-02-15 19:43:17 +1100

    added social icons to menu

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './navbar.styles.scss';
 import MenuContents from '../menu-contents/menu-contents.component';
-import { TimelineLite } from 'gsap';
+import { TimelineLite, TweenLite } from 'gsap';
 import { withRouter, Link } from 'react-router-dom';
 import { Sling as Hamburger } from 'hamburger-react';
+
+import { FaInstagram, FaLinkedin, FaFacebook, FaYoutube } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
 
 function Navbar() {
 	const [sidebar, setSidebar] = useState(false);
@@ -12,6 +15,7 @@ function Navbar() {
 
 	let menuBar = useRef(null);
 	let logo = useRef(null);
+	let socialIcons = useRef(null);
 	let firstLoad = false;
 
 	let tl = new TimelineLite();
@@ -25,9 +29,11 @@ function Navbar() {
 		if (sidebar) {
 			tl.to(menuBar, 1, { css: { display: 'block', opacity: 1 } });
 			tl2.to(logo, 1, { opacity: 0 });
+			TweenLite.to(socialIcons, 0.5, { opacity: 0 });
 		} else {
 			tl2.to(logo, 0.2, { opacity: 1 });
 			tl.to(menuBar, 1, { css: { display: 'none', opacity: 0 } });
+			TweenLite.to(socialIcons, 0.5, { opacity: 1 });
 		}
 	}, [sidebar]);
 	return (
@@ -42,13 +48,35 @@ function Navbar() {
 					className="still-image"
 					src={require(`../../assets/t15_logo.png`)}></img>
 			</Link>
-			<div className="navbar" onClick={() => setSidebar(!sidebar)}>
-				<Hamburger
-					toggled={sidebar}
-					toggle={setSidebar}
-					color="#FFFFFF"
-					duration={0.8}
-				/>
+			<div className="navbar">
+				<IconContext.Provider
+					value={{
+						color: 'white',
+						className: 'social-icons',
+						size: '24px',
+					}}>
+					<div className="icons-container" ref={(el) => (socialIcons = el)}>
+						<a href="https://www.instagram.com/t15_media/" target="_blank">
+							<FaInstagram />
+						</a>
+						<a
+							href="https://www.youtube.com/channel/UCiSd79_k_E8nyE6PcUBmqSw"
+							target="_blank">
+							<FaYoutube />
+						</a>
+						<a href="https://www.facebook.com/T15Productions/" target="_blank">
+							<FaFacebook />
+						</a>
+					</div>
+				</IconContext.Provider>
+				<div className="burger-container" onClick={() => setSidebar(!sidebar)}>
+					<Hamburger
+						toggled={sidebar}
+						toggle={setSidebar}
+						color="#FFFFFF"
+						duration={0.8}
+					/>
+				</div>
 			</div>
 		</div>
 	);
